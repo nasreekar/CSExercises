5a4ffdbc0db5a3eb3473344eca19953658bbf7d9
nasreekar <nasreekar@gmail.com>
2015-08-14 17:10:38 +0800

    Finished

--- CSExercises/Ex07-15/Ex10.cs
+++ CSExercises/Ex07-15/Ex10.cs
@@ -10,7 +10,17 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter x1");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y1");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter x2");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter y2");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double dist = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+            Console.WriteLine("Distance between the two points is {0:#.###}", dist);
         }
     }
 }
