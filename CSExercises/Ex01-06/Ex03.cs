3dd4a111f9bdb1dcf4858648459c65d26ad45d11
nasreekar <nasreekar@gmail.com>
2015-08-14 14:29:37 +0800

    Finished

--- CSExercises/Ex01-06/Ex03.cs
+++ CSExercises/Ex01-06/Ex03.cs
@@ -10,7 +10,9 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter the decimal number");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Square of the number is {0:#.##}", x*x);
         }
     }
 }
