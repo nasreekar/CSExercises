335d64cc0438d12deb1eee43d066bf918ebe4c3b
nasreekar <nasreekar@gmail.com>
2015-08-17 11:36:58 +0800

    Finished

--- CSExercises/Ex07-15/Ex14.cs
+++ CSExercises/Ex07-15/Ex14.cs
@@ -1,10 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
 namespace CSExercises
 {
     public class Ex14
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter the sides of the triangle");
+            Double a = Convert.ToDouble(Console.ReadLine());
+            Double b = Convert.ToDouble(Console.ReadLine());
+            Double c = Convert.ToDouble(Console.ReadLine());
+
+            Double s = (a + b + c) / 2;
+            Double Area = Math.Sqrt((s * (s - a) * (s - b) * (s - c)));
+            Console.WriteLine("the area of the triangle is {0:0.000}", Area);
+
+            Console.ReadLine();
         }
     }
 }
--- CSExercises/Ex07-15/Ex15.cs
+++ CSExercises/Ex07-15/Ex15.cs
@@ -1,10 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
 namespace CSExercises
 {
     public class Ex15
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter any integer between 100-999");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int sum=0;
+            int len = num.ToString().Length;
+
+            if (len == 3)
+            {
+                while (num != 0)
+                {
+                    int a = num / 100;
+                    int b = (num - (a * 100)) / 10;
+                    int c = (num - (a * 100) - (b * 10));
+
+                    sum = ((a * a * a) + (b * b * b) + (c * c * c));
+
+                    if (sum == num)
+                    {
+                        Console.WriteLine("the number {0} is an armstrong number", num);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("the number is not an armstrong number");
+                        break;
+                    }
+                }
+            }
+
+            else
+                Console.WriteLine("Enter a number which matches the requirements");
         }
     }
 }
