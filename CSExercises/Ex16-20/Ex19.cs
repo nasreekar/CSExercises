7515d40232fc4bbeac563813f5325d51d2d17f3b
nasreekar <nasreekar@gmail.com>
2015-08-17 16:25:56 +0800

    Finished

--- CSExercises/Ex16-20/Ex19.cs
+++ CSExercises/Ex16-20/Ex19.cs
@@ -12,12 +12,22 @@ namespace CSExercises
         {
             Console.WriteLine("Enter the total distance travelled");
             Double km = Convert.ToDouble(Console.ReadLine());
-            km = km * 1000;
-            km = Math.Ceiling(km / 100);
+           Double minc = 2.4;
+            Double totalfare;
 
-            double minc = 2.4;
-            double totalfare = 2.4  + (85 * 0.04) + ((km - 90) * 0.05);
-
+            if (km>=9)
+            {
+                km = km * 1000;
+                km = Math.Ceiling(km / 100);
+                totalfare = 2.4 + (85 * 0.04) + ((km - 90) * 0.05);
+            }
+            else
+            {
+                km = km * 1000;
+                km = Math.Ceiling(km / 100);
+                totalfare = 2.4 + ((km-5) * 0.04);
+            }
+                
             Console.WriteLine("Total fare of the trip is ${0:0.00}", totalfare);
             Console.ReadLine();
 
