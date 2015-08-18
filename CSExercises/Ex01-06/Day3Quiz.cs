772bfe75441dc336c283777dfa55980bff6468d6
nasreekar <nasreekar@gmail.com>
2015-08-18 10:08:43 +0800

    Finished

---
+++ CSExercises/Ex01-06/Day3Quiz.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSExercises
+{
+    public class Day3Quiz
+    {
+        public static void Main(string[] args)
+        {
+            Double pin = 123456;
+            Console.WriteLine("Welcome to the Bank of ISS");
+            int counter = 0;
+
+            for (int i=1; i<=3;i++)
+            {
+                Console.WriteLine("Enter your PIN");
+                Double x = Convert.ToDouble(Console.ReadLine());
+
+                if(x!=pin)
+                {
+                    Console.WriteLine("Incorrect Pin. Please try again");
+                    counter = counter + 1;
+                }
+                else
+                {
+                    Console.WriteLine("Correct PIN. You can access your account now");
+                    return;
+                }
+            }
+
+            if (counter==3)
+            {
+                Console.WriteLine("Too Many Wrong PIN entries. Your account is locked");
+            }
+            Console.Read();
+        }
+    }
+}
