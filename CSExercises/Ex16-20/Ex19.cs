a4cadee7e8bd071539b147c710d923ab54b5c84d
nasreekar <nasreekar@gmail.com>
2015-08-20 13:44:51 +0800

    Finished

--- CSExercises/Ex16-20/Ex19.cs
+++ CSExercises/Ex16-20/Ex19.cs
@@ -19,13 +19,13 @@ namespace CSExercises
             {
                 km = km * 1000;
                 km = Math.Ceiling(km / 100);
-                totalfare = 2.4 + (85 * 0.04) + ((km - 90) * 0.05);
+                totalfare = minc + (85 * 0.04) + ((km - 90) * 0.05);
             }
             else
             {
                 km = km * 1000;
                 km = Math.Ceiling(km / 100);
-                totalfare = 2.4 + ((km-5) * 0.04);
+                totalfare = minc + ((km-5) * 0.04);
             }
                 
             Console.WriteLine("Total fare of the trip is ${0:0.00}", totalfare);
